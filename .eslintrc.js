1ff8eb090c8a8f5aaf94e7c70393619c6f553b1f
Rony Amorim dos Reis <rony@smartstaff.com.br>
2025-12-08 22:32:36 -0300

    chore: configure ESLint with TypeScript support

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  env: {
+    node: true,
+    es6: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
